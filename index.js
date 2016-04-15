dc87c16286400abf3e3b020eb9d654d954473df6
Regis <boudinot.regis@yahoo.com>
2016-04-15 17:45:39 -0600

    more ES6 - keeping data for visualization

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-var five = require("johnny-five");
-var board = new five.Board();
+'use strict'
 
-// The board's pins will not be accessible until
-// the board has reported that it is ready
-board.on("ready", function() {
-  console.log("Ready!");
+const five = require("johnny-five")
+const Selfup = require('selfup-rejs')
+const rejs = new Selfup
+let board, button
 
-  var led = new five.Led(13);
-  led.blink(500);
-});
+board = new five.Board();
+
+board.on("ready", () => {
+  rejs.createTable('buttonLog')
+  button = new five.Button(2);
+
+  board.repl.inject({
+    button: button
+  })
+
+  button.on("down", () => {
+    rejs.newData('buttonLog', {buttonStatus: "down"})
+    console.log("down")
+  })
+
+  button.on("hold", () => {
+    rejs.newData('buttonLog', {buttonStatus: "hold"})
+    console.log("hold")
+  })
+
+  button.on("up", () => {
+    rejs.newData('buttonLog', {buttonStatus: "up"})
+    console.log("up")
+  })
+})
